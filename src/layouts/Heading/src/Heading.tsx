3c3faf25cf1d1d69fa9998e3f90734d4bf5b4659
matheoht <108397640+matheoht@users.noreply.github.com>
2022-08-24 09:17:09 +0200

    refactor: Heading component

--- src/layouts/Heading/src/Heading.tsx
+++ src/layouts/Heading/src/Heading.tsx
@@ -1,60 +1,32 @@
 import React, { ReactNode } from "react";
-import "./Heading.css";
 
 export interface HeadingProps {
   children?: ReactNode;
-  type: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
-  size?: string;
-  color?: string;
+  type?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+  color?: "red" | "orange" | "blue" | "green" | "purple" | "gray";
+  style?: {};
   onClick?: () => void;
-  m?: string;
-  ml?: string;
-  mt?: string;
-  mr?: string;
-  mb?: string;
-  p?: string;
-  pl?: string;
-  pt?: string;
-  pr?: string;
-  pb?: string;
 }
 
 const Heading: React.FC<HeadingProps> = ({
   type = "h1",
+  color,
+  style,
   children,
   onClick,
-  size,
-  color,
-  m,
-  ml,
-  mt,
-  mr,
-  mb,
-  p,
-  pl,
-  pt,
-  pr,
-  pb,
 }) => {
+  let colorClass;
+  if (color) {
+    colorClass = "color-primary-" + color;
+  }
+
   // React.createElement() method is used instead JSX to avoid an if/else trees or switch by passing directly the type as argument of React.createElement()
   return React.createElement(
     type,
     {
+      className: colorClass,
+      style: style,
       onClick,
-      style: {
-        color,
-        fontSize: size,
-        margin: m,
-        marginLeft: ml,
-        marginTop: mt,
-        marginRight: mr,
-        marginBottom: mb,
-        padding: p,
-        paddingLeft: pl,
-        paddingTop: pt,
-        paddingRight: pr,
-        paddingBottom: pb,
-      },
     },
     children,
   );
