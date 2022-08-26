fd5db6135cc5c070fe129569159d6c7a9fc24c25
matheoht <108397640+matheoht@users.noreply.github.com>
2022-08-26 09:56:28 +0200

    refactor: Heading component

--- src/layouts/Heading/src/Heading.tsx
+++ src/layouts/Heading/src/Heading.tsx
@@ -4,31 +4,23 @@ export interface HeadingProps {
   children?: ReactNode;
   type?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
   color?: "red" | "orange" | "blue" | "green" | "purple" | "gray";
-  style?: {};
+  style?: React.CSSProperties;
   onClick?: () => void;
 }
 
 const Heading: React.FC<HeadingProps> = ({
-  type = "h1",
+  type = "h1", // Set default type
   color,
   style,
   children,
   onClick,
 }) => {
-  //if a color is provided use color-primary-{color} otherwise use the default black color of the library
-  let colorClass;
-  if (color) {
-    colorClass = "DS-color-primary-" + color;
-  } else {
-    colorClass = "DS-color-black";
-  }
 
-  // React.createElement() method is used instead JSX to avoid an if/else trees or switch by passing directly the type as argument of React.createElement()
-  return React.createElement(
+  return React.createElement(// React.createElement() method is used instead JSX to avoid an if/else trees or switch by passing directly the type as argument of React.createElement()
     type,
     {
-      className: colorClass,
-      style: style,
+      className: color ? `DS-color-primary-${color} ` : "DS-color-black ", // If a color is provided use DS-color-primary-{color} otherwise use the default black color of the library
+      style,
       onClick,
     },
     children,
