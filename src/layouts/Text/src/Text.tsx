31a2f5a911710b51326bde3a0ead699148c1667a
matheoht <108397640+matheoht@users.noreply.github.com>
2022-08-27 20:33:15 +0200

    feat: Add Text component

---
+++ src/layouts/Text/src/Text.tsx
@@ -0,0 +1,39 @@
+import React, { ReactNode } from "react";
+import "./Text.css";
+
+export interface TextProps {
+  color?: "red" | "orange" | "blue" | "green" | "purple" | "gray";
+  size?: "s" | "md" | "l";
+  weight?: "light" | "regular" | "bold";
+  italic?: Boolean;
+  children?: ReactNode;
+  style?: React.CSSProperties;
+  onClick?: () => void;
+}
+
+const Text: React.FC<TextProps> = ({
+  color,
+  size = "md", // Set default size
+  weight = "regular", // Set default weight
+  italic = false, // Set default italic to false
+  style,
+  children = "Default name", // Set default name 
+  onClick,
+}) => {
+  return (
+    <p
+      className={
+        (color ? `DS-color-primary-${color} ` : "DS-color-black ") + // If a color is provided use DS-color-primary-{color} otherwise use the default black color of the library
+        (size ? `Text-size-${size} ` : "") + // Set the size
+        (weight ? `Text-weight-${weight} ` : "") + // Set the weight
+        (italic ? "Text-italic" : "") // If italic is provided add the class "Text-italic"
+      }
+      style={style}
+      onClick={onClick}
+    >
+      {children}
+    </p>
+  );
+};
+
+export default Text;
