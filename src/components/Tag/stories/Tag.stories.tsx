367c1b5ed76bbfdcb043d324cd25504c6f412b98
matheoht <108397640+matheoht@users.noreply.github.com>
2022-08-29 21:33:44 +0200

    chore: Add Tag story

---
+++ src/components/Tag/stories/Tag.stories.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import Tag from "../src/Tag";
+
+export default {
+  title: "Components/Tag",
+  component: Tag,
+  argTypes: { italic: { control: 'boolean' }}
+} as ComponentMeta<typeof Tag>;
+
+const Template: ComponentStory<typeof Tag> = (args) => (
+  <Tag
+    color={args.color}
+    size={args.size}
+    radius={args.radius}
+    style={args.style}
+    onClick={args.onClick}
+  >
+    {args.children}
+  </Tag>
+);
+
+export const text = Template.bind({});
+
+text.args = {
+  size: "md",
+  radius: "default",
+  children: "Default Tag",
+};
+
