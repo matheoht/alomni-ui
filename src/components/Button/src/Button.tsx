636509be8b2fbcc96c6263298e77bedf0aba5b2c
matheoht <108397640+matheoht@users.noreply.github.com>
2022-09-01 19:39:35 +0200

    chore: Add Button component

---
+++ src/components/Button/src/Button.tsx
@@ -0,0 +1,46 @@
+import React, { ReactNode } from "react";
+import "./Button.css";
+
+export interface ButtonProps {
+  color?: "red" | "orange" | "blue" | "green" | "purple" | "gray" | "black";
+  type?: "solid" | "outline";
+  size?: "s" | "md" | "l";
+  radius?: "sharp" | "default" | "rounded";
+  width?: "default" | "full";
+  disabled?: Boolean;
+  style?: React.CSSProperties;
+  children?: ReactNode;
+  onClick?: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({
+  color = "blue", // Set default color
+  type = "solid", // Set default type
+  size = "md", // Set default size
+  radius = "default", // Set default radius
+  width = "default", // Set default width
+  disabled,
+  style,
+  children = "Default name", // Set default name
+  onClick,
+}) => {
+  return (
+    <button
+      disabled={!!disabled}
+      className={
+        "Button " +
+        (color ? `Button-${type}-${color} ` : "") + // Set type and color
+        (size ? `Button-size-${size} ` : "") + // Set size
+        (radius ? `Button-radius-${radius} ` : "") + // Set radius
+        (width ? `Button-width-${width} ` : "") // Set width
+      }
+      style={style}
+      onClick={onClick}
+      type="button"
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
