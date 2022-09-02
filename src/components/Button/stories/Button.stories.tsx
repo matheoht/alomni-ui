0f2885c472bdd4e1ea86ccad9ca9f6e4132c8285
matheoht <108397640+matheoht@users.noreply.github.com>
2022-09-02 10:18:44 +0200

    chore: Add story to Button component

---
+++ src/components/Button/stories/Button.stories.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+
+import Button from "../src/Button";
+
+export default {
+  title: "Components/Button",
+  argTypes: { 
+    disabled: { control: 'boolean' }
+  },
+  component: Button,
+} as ComponentMeta<typeof Button>;
+
+const Template: ComponentStory<typeof Button> = (args) => (
+  <Button
+    color={args.color}
+    type={args.type}
+    size={args.size}
+    radius={args.radius}
+    width={args.width}
+    disabled={args.disabled}
+    style={args.style}
+    onClick={args.onClick}
+  >
+    {args.children}
+  </Button>
+);
+
+export const button = Template.bind({});
