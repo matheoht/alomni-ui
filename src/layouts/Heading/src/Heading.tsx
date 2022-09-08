1c5a08bdb64604cc16b9f1f155ee2ce766d323a4
matheoht <108397640+matheoht@users.noreply.github.com>
2022-09-08 21:07:57 +0200

    style: formating Heading component

--- src/layouts/Heading/src/Heading.tsx
+++ src/layouts/Heading/src/Heading.tsx
@@ -12,11 +12,11 @@ const Heading: React.FC<HeadingProps> = ({
   type = "h1", // Set default type
   color,
   style,
-  children = "Default name", // Set default name 
+  children = "Default name", // Set default name
   onClick,
 }) => {
-
-  return React.createElement(// React.createElement() method is used instead JSX to avoid an if/else trees or switch by passing directly the type as argument of React.createElement()
+  return React.createElement(
+    // React.createElement() method is used instead JSX to avoid an if/else trees or switch by passing directly the type as argument of React.createElement()
     type,
     {
       className: color ? `DS-color-primary-${color} ` : "DS-color-black ", // If a color is provided use DS-color-primary-{color} otherwise use the default black color of the library
